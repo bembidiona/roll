3f9c756a8af109e23535e99008a3c8dd4d1688fb
jerebab <jerebab@localhost>
2016-01-23 15:46:19 -0300

    more stuff

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 var trainX = 0;
-var speedX = 5;
 var trainR = 0;
-var speedR = 5;
-var sliderX;
-var sliderR;
+var maxX = 20;
+var maxR = 5;
+var sliderX = 0;
+var sliderR = 0;
 
 var pointXOld;
 var mouseYOld;
@@ -31,12 +31,18 @@ var canvasHeight;
 
 var buttonSize = 40;
 var buttons = [];
+var sliders = [];
 var buttonsTypes = ["paint", "erase", "clear", "save", "stop"];
 var btnImg_paint;
 var btnImg_erase;
 var btnImg_clear;
 var btnImg_stop;
 var btnImg_save;
+var btnImg_rtrain;
+var btnImg_xtrain;
+var sliderSize = 150;
+
+var collisionPoints = [];
 
 
 var currentTool = "paint";
@@ -52,27 +58,23 @@ function setup() {
 	var c = createCanvas(windowWidth, windowHeight, "2d");
 	c.drop(gotFile); 
 
-	canvasWidth = windowWidth - uiWidth;
+	canvasWidth = windowWidth;
 	canvasHeight = windowHeight;
 
 	createMiniCanvases();
 
-	sliderX = createSlider(-600, 600, 200);
-  	sliderX.position(margen, margen);
-  	sliderX.style('width', '60px');
-
-  	sliderR = createSlider(-200, 200, 0);
-  	sliderR.position(margen, margen*3);
-  	sliderR.style('width', '60px');
-
+	
   	angleMode(DEGREES);
 
   	btnImg_paint = loadImage("img/brush.png");
-	btnImg_erase = loadImage("img/erase.png");
-	btnImg_clear = loadImage("img/clear.png");
+	btnImg_erase = loadImage("img/eraser.png");
+	btnImg_clear = loadImage("img/checkbox-blank.png");
 	btnImg_stop = loadImage("img/stop.png");
 	btnImg_save = loadImage("img/save.png");
-	
+	btnImg_stop = loadImage("img/stop.png");
+	btnImg_save = loadImage("img/save.png");
+	btnImg_rtrain = loadImage("img/cached.png");
+	btnImg_xtrain = loadImage("img/code-tags.png");
 
   
 
@@ -85,6 +87,10 @@ function setup() {
     	if(i <= 3) buttons.push(new Boton(margen, i * (buttonSize + margen) + margen, buttonsTypes[i]));
     	else buttons.push(new Boton(margen, windowHeight - margen - buttonSize, buttonsTypes[i]));  	
     }
+
+    sliders.push(new Slider(margen, windowHeight - margen*2 - buttonSize*2, false, "rtrain"));  	
+    sliders.push(new Slider(margen + buttonSize + margen, windowHeight - margen - buttonSize, true, "xtrain"));
+
 }
 
 function draw() {  
@@ -92,7 +98,7 @@ function draw() {
 
 	pg0 = pg2 = pg1;
 
-
+	push();
 	translate(canvasWidth/2, canvasHeight/2);
 	rotate(trainR);
 	translate(-canvasWidth/2, -canvasHeight/2);
@@ -103,7 +109,7 @@ function draw() {
 
 	
   	
-	if (mouseIsPressed && mouseX > uiWidth){
+	if (mouseIsPressed && !isOnUI()){
 
 		var pointX;		
 
@@ -141,44 +147,44 @@ function draw() {
 		jumpTestOld = jumpTest;
 	}
 	
-	if(sliderX.value() > 0){
-		if(trainX < canvasWidth) trainX += speedX;
+	if(sliderX > 0){
+		if(trainX < canvasWidth) trainX += sliderX;
 		else trainX = 0;
 	}
-	if (sliderX.value() < 0){
-		if(trainX > -canvasWidth) trainX += speedX;
+	if (sliderX < 0){
+		if(trainX > -canvasWidth) trainX += sliderX;
 		else trainX = 0;
 	}
 
-	if(sliderR.value() != 0){		
-		trainR = trainR + sliderR.value()*0.01;
+	if(sliderR != 0){		
+		trainR = trainR + sliderR;
 	}
 
-	translate(canvasWidth/2, canvasHeight/2);
-	rotate(-trainR);
-	translate(-canvasWidth/2, -canvasHeight/2);	
+	
 
-	speedX = sliderX.value() * 0.01;
 
-	//draw line
-	strokeWeight(2);
-	stroke(cPlayer);
-	line(canvasWidth/2 + uiWidth,0,canvasWidth/2 + uiWidth,canvasHeight);
-	stroke(cLines);
-	//---
+
+	
+	pop();
+
+	var listenerImg = get(canvasWidth/2 - 1, 0, 2, canvasHeight);
 
 	//UI
-	/*noStroke();
-	fill(255);
-	rect(0,0,uiWidth,windowHeight);*/
-
+	strokeWeight(2);
+	stroke(cPlayer);
+	line(canvasWidth/2,0,canvasWidth/2,canvasHeight);
+	stroke(cLines);
+	
 	for (var i=0; i < buttons.length; i++) {
     	buttons[i].display();    	
+    }
+    for (var i=0; i < sliders.length; i++) {
+    	sliders[i].display();    	
     }
 
 	//----SOUND
 
-	var listenerImg = get(canvasWidth/2 + uiWidth + 4, 0, 3, canvasHeight);
+	
 	var pix;
 	var safeWait = 0;
 	var voiceNum = 0;
@@ -192,13 +198,18 @@ function draw() {
 			pix = listenerImg.get(2,i-1);
 
 			if(pix[1] == 0){
-				safeWait = 40; //safe
+				safeWait = 20; //safe
 
 				var f = canvasHeight - i;
 				oscFreq = ( 550 * f ) / canvasHeight + 150;
 				
 				oscilatorsFreq[voiceNum] = oscFreq;
 				voiceNum++;
+
+				var lol = 14;
+				fill(0);
+				noStroke();
+				rect(windowWidth/2 - lol/2, i - lol/2, lol, lol);
 				
 			} 
 		}
@@ -233,26 +244,39 @@ function createMiniCanvases() {
 
 	pg1.background(cBg);
 	pg1.stroke(220);
-	for (var i=0; i < pg1.width; i+= 100){
+	for (var i=0; i < pg1.width; i+= 80){
 		pg1.line(i, 0, i, canvasHeight);
 	}
 	pg1.stroke(200);
-	for (var i=0; i < pg1.height; i+= 50){
+	for (var i=0; i < pg1.height; i+= 40){
 		pg1.line(0, i, canvasWidth, i);
 	}
 	pg0 = pg2 = pg1;
 }
 
+function isOnUI() {
+	if (mouseX < uiWidth) return true;
+	else if(mouseX < uiWidth + sliderSize + buttonSize && mouseY > windowHeight - buttonSize - margen) return true;
+	else return false;
+}
+
 function mousePressed() {
-  	if(mouseX < uiWidth){
+  	if(isOnUI){
 		for (var i=0; i < buttons.length; i++) {
 	    	buttons[i].checkClick();    	
+	    }
+	    for (var i=0; i < sliders.length; i++) {
+	    	sliders[i].checkClick();    	
 	    }
 	}
 }
 
 function mouseReleased() {
   firstNode = true;
+
+    for (var i=0; i < sliders.length; i++) {
+    	sliders[i].isBeingDragged = false;    	
+    }
 }
 
 // Oscilator Class
@@ -263,6 +287,117 @@ function Oscilador() {
 
     this.osc = new p5.SqrOsc(220);
     this.osc.start();    
+}
+
+function Slider(_x, _y, _isHorizontal, _tipo) {
+    this.x = _x;
+    this.y = _y; 
+    this.w = buttonSize;
+    this.h = buttonSize;
+    this.isHorizontal = _isHorizontal;
+    this.tipo = _tipo;   
+    this.icon;
+    this.isBeingDragged = false;
+    this.min;
+    this.max;
+    this.grabPoint;
+
+    if(this.isHorizontal){
+    	this.min = this.x; 
+    	this.max = this.x + sliderSize;
+    }
+    else{
+    	this.min = this.y; 
+    	this.max = this.y - sliderSize;
+    }
+
+    if(this.tipo == "rtrain") this.icon = btnImg_rtrain;
+	else if(this.tipo == "xtrain") this.icon = btnImg_xtrain;   
+
+	
+
+	this.reset = function(){
+	 	if(this.isHorizontal){
+	 		this.x = this.min + sliderSize/2;
+	 	}
+	 	else{
+	 		this.y = this.min - sliderSize/2;
+	 	}
+	}
+
+	this.reset();
+
+    this.display = function() {
+
+    	if(this.isBeingDragged){
+
+    		if(this.isHorizontal){
+    			this.x = mouseX - this.grabPoint;
+
+    			if(this.x > this.max) this.x = this.max - 1;
+    			else if (this.x < this.min) this.x = this.min;
+
+
+    			sliderX = ((this.x-this.min) * maxX) / sliderSize;
+    			
+    			if (sliderX < maxX/2) sliderX = (maxX - sliderX)*-1 + maxX/2;
+    			else if (sliderX > maxX/2) sliderX -= maxX/2;
+    			else sliderX = 0;
+    			
+    		}
+    		else{
+    			this.y = mouseY - this.grabPoint;
+
+    			if(this.y < this.max) this.y = this.max;
+    			else if (this.y > this.min) this.y = this.min;
+
+    			sliderR = ((this.y-this.min) * maxR) / sliderSize *-1;
+    			
+    			if (sliderR < maxR/2) sliderR = (maxR - sliderR)*-1 + maxR/2;
+    			else if (sliderR > maxR/2) sliderR -= maxR/2;
+    			else sliderR = 0;
+
+
+    		}
+    	}
+
+    	strokeWeight(5);
+    	stroke(0);
+
+    	if(this.isHorizontal){
+    		line(this.min + this.w/2, this.y + this.h/2, this.min + this.w/2 + sliderSize, this.y + this.h/2);
+    	}
+    	else{
+    		line(this.x + this.w/2, this.min + this.h/2, this.x + this.w/2, this.min + this.h/2 - sliderSize);	
+    	}
+
+
+    	noStroke()
+    	fill(0, 10);
+    	rect(this.x + 5, this.y + 5, this.w, this.h); 
+
+    	if(this.checkMouse()) fill(255,0,0);
+    	else fill(0);
+
+    	rect(this.x, this.y, this.w, this.h);
+
+    	image(this.icon, this.x + buttonSize/2 - this.icon.width/2, this.y + buttonSize/2 - this.icon.height/2);
+    }
+
+    this.checkMouse = function() {
+    	if (this.x < mouseX && mouseX < this.x + this.w && this.y < mouseY && mouseY < this.y + this.h) return true;
+    	else return false;    	
+    }
+
+    this.checkClick = function() {
+    	if(this.checkMouse()){
+    		 this.isBeingDragged = true;	
+
+    		 if (this.isHorizontal) this.grabPoint = mouseX - this.x;
+    		 else this.grabPoint = mouseY - this.y; 	
+    	}	
+    }
+
 }
 
 function Boton(_x, _y, _tipo) {
@@ -277,12 +412,13 @@ function Boton(_x, _y, _tipo) {
     else if(this.tipo == "erase") this.icon = btnImg_erase;
     else if(this.tipo == "clear") this.icon = btnImg_clear;
 	else if(this.tipo == "stop") this.icon = btnImg_stop;
-	else if(this.tipo == "save") this.icon = btnImg_save;    
+	else if(this.tipo == "save") this.icon = btnImg_save;
+	     
 
 
     this.display = function() {
     	noStroke()
-    	fill(0, 30);
+    	fill(0, 10);
     	rect(this.x + 5, this.y + 5, this.w, this.h); 
 
     	if(this.checkMouse()) fill(255,0,0);
@@ -310,8 +446,12 @@ function Boton(_x, _y, _tipo) {
     			createMiniCanvases();
     		}
     		else if(this.tipo == "stop"){
-    			sliderX.value(0);
-    			sliderR.value(0);
+
+    			for (var i=0; i < sliders.length; i++) {
+    				sliders[i].reset();    	
+   				}
+   				sliderX = 0;
+   				sliderR = 0;
     			trainX = 0;
     			trainR = 0;
     		}
